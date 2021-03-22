574e3f4cf932857cabac32545c9049a6663e9e25
Abhiram K <abhiram@cklabs.com>
2021-03-22 11:52:49 +0530

    Added General Structure

---
+++ Appium/src/main/java/net/training-support/sdet/App.java
@@ -0,0 +1,13 @@
+package net.training-support.sdet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
